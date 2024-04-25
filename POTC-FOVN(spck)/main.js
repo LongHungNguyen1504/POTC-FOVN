0806aa46431b1b2326a20a2d2bdb244d6194a36c
minhkhangnguyen123 <minhkhangswd@gmail.com>
2024-04-25 20:18:07 +0700

    slide, fetch API

--- POTC-FOVN(spck)/main.js
+++ POTC-FOVN(spck)/main.js
@@ -1,1 +1,65 @@
-//
+const filePath = 'data.json';
+
+function activeSlide() {
+
+    //service section owl carousel
+    $("m-post").owlCarousel({
+        autoplay: true,
+        loop: true,
+        margin: 20,
+        autoHeight: true,
+        nav: true,
+        navText: [
+            '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
+            '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>',
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            991: {
+                items: 4,
+            },
+        },
+    });
+}
+let titlearea = document.getElementById('poster')
+function createItemElement(itemData) {
+    itemData && itemData.map((item) => {
+        let itemTemplate = `  
+            <div id="m-post" style="width : 326px; background-color : black;">
+                <img src="${item.image}" alt="">
+
+                <div class="p-title">
+                    <h6 style="">${item.title}</h6>
+
+                </div>
+                <div>
+                    <p style="color : white">
+                    ${item.text}
+                    </p>
+                </div>
+            </div>
+        `;
+        titlearea.innerHTML += itemTemplate;
+    });
+}
+init()
+
+function init() {
+    getPlayerAPI()
+}
+
+async function getPlayerAPI() {
+    let data = await fetch(filePath)
+        .then(response => response.json()) // lấy dữ liệu từ fetch('link j đó)lưu vào data
+        .then(json => json)//chuyển response thành json sau đó đổi thành array/object và lưu vào biến data
+    console.log(data)
+    createItemElement(data)
+}
